be81281d2c1fa29e3ee57670b132d25ffaaf45d0
Thomas Sojka <thomas.sojka@comsysto.com>
2019-03-03 21:12:00 +0100

    Refactor: Reuse sketch code

--- src/sketches/core.cljs
+++ src/sketches/core.cljs
@@ -13,45 +13,22 @@
    [:div.mv3
     [:span.ma0 title]]])
 
-(defn sketch [title children]
+(defn sketch [title run-sketch]
   (let [isStarted (r/atom false)]
     (fn []
       [card title
        (if @isStarted
-         [children]
+         (let [canvas-id (str (random-uuid))]
+           [(with-meta (fn [] [:canvas.w-100.br-100.ba.b--gray {:id canvas-id}])
+              {:component-did-mount #(run-sketch canvas-id)})])
          [:div.h-100.w-100.br-100.flex.justify-center.items-center.bg-gray
           [:button.button.bg-transparent.bn.white.pointer {:on-click #(reset! isStarted true)} "▶"]])])))
-
-(defn paint-splatter []
-  (let [canvas-id "paint-splatter"]
-    [(with-meta (fn [] [:canvas.w-100.br-100.ba.b--gray {:id canvas-id}])
-       {:component-did-mount #(p/run-paint-splatter canvas-id)})]))
-
-(defn random-walk []
-  (let [canvas-id "random-walk"]
-    [(with-meta (fn [] [:canvas.w-100.br-100.ba.b--gray {:id canvas-id}])
-       {:component-did-mount #(rw/run-random-walk canvas-id)})]))
-
-(defn fire []
-  (let [canvas-id "fire"]
-    [(with-meta (fn [] [:canvas.w-100.br-100.ba.b--gray {:id canvas-id}])
-       {:component-did-mount #(f/run-fire canvas-id)})]))
-
-(defn spiral []
-  (let [canvas-id "spiral"]
-    [(with-meta (fn [] [:canvas.w-100.br-100.ba.b--gray {:id canvas-id}])
-       {:component-did-mount #(s/run-spiral canvas-id)})]))
-
-(defn noise []
-  (let [canvas-id "noise"]
-    [(with-meta (fn [] [:canvas.w-100.br-100.ba.b--gray {:id canvas-id}])
-       {:component-did-mount #(n/run-noise canvas-id)})]))
 
 (defn app []
   [:main.sans-serif.pa4.white
    [:div.mb5
     [:h1 "Nature of Code - Exercises"]
-    [:p.f4 "Exercises of "
+    [:p.f4.lh-title "Exercises of "
      [:a.pointer.bb.link.white {:href "https://twitter.com/shiffman"} "Daniel Shiffman's"] " "
      [:a.pointer.bb.link.white {:href "https://natureofcode.com/"} "Nature of Code"]
      " implemented in ClojureScript."]
@@ -59,9 +36,9 @@
    [:div
     [:h2.tracked "Introduction"]
     [:div.flex.flex-wrap.justify-center.justify-start-ns
-     [sketch "Random Walk" random-walk]
-     [sketch "Paint Splatter" paint-splatter]
-     [sketch "Noise" noise]
+     [sketch "Random Walk" rw/run-random-walk]
+     [sketch "Paint Splatter" p/run-paint-splatter]
+     [sketch "Noise" n/run-noise]
      [card "in progress" "🚧"]]]
    [:div
     [:h2.tracked "Vectors"]
@@ -70,12 +47,12 @@
    [:div
     [:h2.tracked "Oscillation"]
     [:div.flex.flex-wrap.justify-center.justify-start-ns
-     [sketch "Spiral" spiral]
+     [sketch "Spiral" s/run-spiral]
      [card "in progress" "🚧"]]]
    [:div
     [:h2.tracked "Particle Systems"]
     [:div.flex.flex-wrap.justify-center.justify-start-ns
-     [sketch "Fire" fire]
+     [sketch "Fire" f/run-fire]
      [card "in progress" "🚧"]]]])
 
 (r/render app (.getElementById js/document "app"))
