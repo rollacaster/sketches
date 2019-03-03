4912b87ca00c35e94af0fea59145e2ba168ba4bd
Thomas Sojka <thomas.sojka@comsysto.com>
2019-03-03 13:28:57 +0100

    Slow spiral rotation

--- src/sketches/spiral.cljs
+++ src/sketches/spiral.cljs
@@ -2,17 +2,21 @@
   (:require [quil.core :as q :include-macros true]
             [sketches.vector :as v]))
 
+(def delta (atom 0))
+
 (defn draw [host]
   (fn []
-    (q/frame-rate 5)
     (.clearRect (.getContext (.getElementById js/document host) "2d") 0 0 (q/width) (q/height))
     (q/stroke 0)
-    (q/rotate (mod (q/frame-count) q/TWO-PI))
+    (q/push-matrix)
+    (q/translate 150 150)
+    (q/rotate (mod (swap! delta + 0.01) q/TWO-PI))
     (q/begin-shape)
     (doseq [x (range 1000)]
-      (q/vertex (+ 150 (* (q/cos x) x))
-                (+ 150 (* (q/sin x) x))))
-    (q/end-shape)))
+      (q/vertex (* (q/cos x) x)
+                (* (q/sin x) x)))
+    (q/end-shape)
+    (q/pop-matrix)))
 
 (defn run-spiral [host]
   (q/defsketch particle
