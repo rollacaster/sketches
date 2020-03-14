6660fca9725926165019af12f6abd3cf2faa0927
Thomas Sojka <thomas@Thomass-MBP.fritz.box>
2020-03-14 20:53:06 +0100

    Rework fire sketch with fun-mode

--- src/sketches/nature_of_code/fire.cljs
+++ src/sketches/nature_of_code/fire.cljs
@@ -1,60 +1,46 @@
 (ns sketches.nature-of-code.fire
   (:require [quil.core :as q :include-macros true]
-            [sketches.nature-of-code.vector :as v]))
+            [quil.middleware :as md]
+            [sketches.mover :as m]))
 
-(def particle-system (atom {:particles () :origin [150 200]}))
+(defn setup []
+  (q/blend-mode :add)
+  {:particles () :origin [150 200]})
 
 (defn create-particle [location]
-  {:location location
-   :velocity [(* (q/random-gaussian) 0.3) (- (* (q/random-gaussian) 0.4) 1.0)]
-   :acceleration [0 0]
-   :lifespan 255.0
-   :mass 10})
+  (-> (m/create-mover 10 location)
+      (assoc :velocity [(* (q/random-gaussian) 0.3) (- (* (q/random-gaussian) 0.4) 1.0)]
+             :lifespan 255.0)))
 
-(defn add-particle [ps]
-  (update ps :particles #(conj % (create-particle (:origin ps)))))
+(defn is-dead [{:keys [lifespan]}] (< lifespan 0.0))
 
-(defn display-particle [{:keys [lifespan] [x y] :location :as particle}]
+(defn update-lifespan [particle] (update particle :lifespan (comp dec dec)))
+
+(defn update-state [ps]
+  (-> ps
+      (update :particles #(conj % (create-particle (:origin ps))))
+      (update :particles #(conj % (create-particle (:origin ps))))
+      (update :particles #(map (comp update-lifespan m/update-mover)  %))
+      (update :particles #(remove is-dead %))))
+
+(defn draw-particle [{:keys [lifespan] [x y] :location}]
   (q/color-mode :rgb)
   (q/fill (q/color 255 24 0 lifespan))
   (q/ellipse x y (q/map-range lifespan 0 255 0 24) (q/map-range lifespan 0 255 0 24)))
 
-(defn is-dead [{:keys [lifespan]}] (< lifespan 0.0))
-
-(defn apply-force [{:keys [mass acceleration] :as particle} force]
-  (assoc particle :acceleration (v/add acceleration (v/div force mass))))
-
-(defn update-particle [{:keys [acceleration velocity location lifespan] :as particle}]
-  (let [velocity (v/add velocity acceleration)
-        location (v/add velocity location)
-        lifespan (- lifespan 2.0)]
-    (-> particle
-        (assoc :velocity velocity)
-        (assoc :location location)
-        (assoc :lifespan lifespan)
-        (assoc :acceleration [0 0]))))
-
-(defn run-particle-system [{:keys [particles confetti] :as ps}]
-  (doseq [particle particles]
-    (display-particle particle))
-  (let [wind [(q/map-range (q/mouse-x) 0.0 (q/width) -0.3 0.3)
-              (q/map-range (q/mouse-y) 0.0 (q/height) -0.3 0.3)]]
-    (-> ps
-        (update :particles #(map (fn [particle] (apply-force particle wind)) %))
-        (update :particles #(map update-particle %))
-        (update :particles #(remove is-dead %)))))
-
-(defn setup [host]
-  (q/blend-mode :add))
-
-(defn draw [host]
+(defn draw [{:keys [particles]}]
   (q/clear)
   (q/background 0)
-  (swap! particle-system (comp add-particle add-particle run-particle-system)))
+  (doseq [particle particles]
+    (draw-particle particle)))
 
 (defn run [host]
-  (q/defsketch particle
+  (q/defsketch fire
     :host host
     :setup setup
     :draw draw
+    :update update-state
+    :middleware [md/fun-mode]
     :size [300 300]))
+
+
