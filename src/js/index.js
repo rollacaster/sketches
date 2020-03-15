703553b34759003a971dead203c88451beca6fe1
Thomas Sojka <thomas@Thomass-MBP.fritz.box>
2020-03-15 08:42:40 +0100

    Use figwheel-main npm shortcut to install deps

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
 import Palettes from 'nice-color-palettes'
 import BezierEasing from 'bezier-easing'
-window.Palettes = Palettes
-window.BezierEasing = BezierEasing
+window['nice-color-palettes'] = Palettes
+window['bezier-easing'] = BezierEasing
